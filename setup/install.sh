1c03f93cd8cd4ce387862286dd95d4995fab3308
Shelby Jueden <shelby@jueden.org>
2018-05-23 21:14:08 -0700

    add base install script and cmake beginings

---
+++ setup/install.sh
@@ -0,0 +1,8 @@
+sudo apt-get -y install subversion gcc texinfo patch libncurses5-dev zlibc zlib1g-dev libx11-dev libusb-dev libreadline6-dev mspdebug
+
+#mkdir /opt/msp430
+#rsync -av --progress msp430-gcc-*_linux64/* /opt/msp430/msp430-gcc
+
+#rsync -av --progress msp430-gcc-support-files /opt/msp430/
+
+
