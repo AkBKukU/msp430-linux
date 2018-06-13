3008016b0de8003c3223d4836bff447e60e5e4ed
Shelby Jueden <shelby@jueden.org>
2018-06-13 16:31:21 -0700

    ignore vim swp

--- exa/cmake/msp430.cmake
+++ exa/cmake/msp430.cmake
@@ -1,32 +1,78 @@
+# CMake toolchain file for MSP430 development
+# by Shelby Jueden (AkBKukU)
+
+
+# Force compiler due to required non-standard flags
+INCLUDE(CMakeForceCompiler)
+
 # Set CMAKE to cross compiling
 set(CMAKE_SYSTEM_NAME Generic)
 
+# Device name for the chip to compile
+set( DEVICE "MSP430FR5969" CACHE STRING "Device part number")
+set( DEVICE_DRVLIB "MSP430FR5xx_6xx" CACHE STRING "DriverLib folder")
+string( TOLOWER ${DEVICE} DEVICE )
+set( LINKER_SCRIPT "${DEVICE}.ld" )
+
 
 # -- Envoironment Configuration --
+set( MSP430_FILES "/opt/msp430" )
 
-# Add TI dependencies assuming directories in opt
-set( GCC_DIR "/opt/msp430/msp430-gcc" )
-set( GCC_SUPPORT_DIR "/opt/msp430/msp430-gcc-support-files" )
-set( DRIVERLIB "/opt/msp430/msp430-driverlib" )
+# Library Include Settings
+set( INC_DRIVERLIB FALSE CACHE BOOL "Include DriverLib" )
+set( INC_ENERGIA FALSE CACHE BOOL "Include Energia(Not Supported Yet)" )
 
-# Executables
-set( GCC_BIN_DIR "${GCC_DIR}/bin" )
-set(CMAKE_C_COMPILER "${GCC_BIN_DIR}/msp430-elf-gcc" )
-set(CMAKE_CXX_COMPILER "${GCC_BIN_DIR}/msp430-elf-g++" )
-set(CMAKE_CXX_LINK_EXECUTABLE "${GCC_BIN_DIR}/msp430-elf-ld" )
-set(CMAKE_C_LINK_EXECUTABLE "${GCC_BIN_DIR}/msp430-elf-ld" )
 
-# search for programs in the build host directories
+# Add GCC files
+set( GCC_DIR "${MSP430_FILES}/msp430-gcc" )
+set( GCC_SUPPORT_DIR "${MSP430_FILES}/msp430-gcc-support-files" )
+
+
+# TI Libraries
+if( INC_DRIVERLIB ) 
+	set( DRIVERLIB_DIR "${MSP430_FILES}/msp430-driverlib/driverlib/${DEVICE_DRVLIB}" )
+endif()
+
+if( INC_ENERGIA ) 
+	#set( ENERGIA_DIR "${MSP430_FILES}/msp430-energia" )
+endif()
+
+
+# GCC Executables
+CMAKE_FORCE_C_COMPILER("${GCC_DIR}/bin/msp430-elf-gcc" GNU)
+CMAKE_FORCE_CXX_COMPILER( "${GCC_DIR}/bin/msp430-elf-g++" GNU)
+
+# Do not use system compiler executables
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-# for libraries and headers in the target directories
+# Only use specified libs
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
+# Compiler flags
+set( CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -T ${LINKER_SCRIPT}" CACHE STRING "" FORCE )
+set( CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -mmcu=${DEVICE}" CACHE STRING "" FORCE )
+set( CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -g -Wl,--gc-sections" CACHE STRING "" FORCE )
+
+set( CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} -O2 -D__$(DEVICE)__ -mmcu=${DEVICE} -g -ffunction-sections" CACHE STRING "" FORCE )
+set( CMAKE_CXX_FLAGS  "${CMAKE_CXX_FLAGS} -fdata-sections -DDEPRECATED" CACHE STRING "" FORCE )
+
+set( CMAKE_C_FLAGS  "${CMAKE_C_FLAGS} -O2 -D__$(DEVICE)__ -mmcu=${DEVICE} -g -ffunction-sections" CACHE STRING "" FORCE )
+set( CMAKE_C_FLAGS  "${CMAKE_C_FLAGS} -fdata-sections -DDEPRECATED" CACHE STRING "" FORCE )
+
+
 
 # include directories
-# TODO - add optional driverlib support
 # TODO - add optional energia support
 include_directories( "${GCC_DIR}/msp430-elf/include" )
 include_directories( "${GCC_SUPPORT_DIR}/include" )
-include_directories( "${DRIVERLIB}" )
+link_directories( "${GCC_SUPPORT_DIR}/include" )
+# TI Libraries
+if( INC_DRIVERLIB ) 
+	include_directories( "${DRIVERLIB_DIR}" )
+	#link_directories( "${DRIVERLIB_DIR}" )
+endif()
+if( INC_ENERGIA ) 
+	include_directories( "${ENERGIA_DIR}" )
+	#link_directories( "${ENERGIA_DIR}" )
+endif()
 
